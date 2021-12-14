48789877e8f87b57fcb073b4283bc62db980081e
future <sgmr0408@gmail.com>
2021-12-14 03:54:14 -0700

    routing for webpage

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Home"
+    return """
+    <!DOCTYPE html>
+    <head>
+        <title>WEB</title>
+    </head>
+    <body>
+        <h1><a href="/index.html">Web</a></h1>
+        <ol>
+            <li><a href="/1.html">html</a></li>
+            <li><a href="/2.html">css</a></li>
+            <li><a href="/3.html">js</a></li>
+        </ol>
+        <h2>Welcome!</h2>
+        Hello, WEB
+    </body>
+    </html>
+    """
 
 @app.route("/read/<id>/")
 def read(id):
