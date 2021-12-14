24aaeb24228cdecfd56b8d8fca129fa0b511c680
future <sgmr0408@gmail.com>
2021-12-14 03:50:42 -0700

    routing several ways

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def hello_world():
     return "Home"
 
-@app.route("/read/1/")
-def read():
-    return "Read"
+@app.route("/read/<id>/")
+def read(id):
+    return "Read"+id
 
-@app.route("/read/2/")
-def read2():
-    return "Read2"
+@app.route("/create/")
+def create():
+    return "create"
 
 app.run(debug=True,host="0.0.0.0",port="8000")
